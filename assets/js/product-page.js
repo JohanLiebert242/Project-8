60694bf3190038e04d72d2ac9a8bf16394382223
JohanLiebert <nguyenloc24022005@gmail.com>
2023-12-14 22:27:09 +0700

    Done Dark mode + Responsive for Product Page

---
+++ assets/js/product-page.js
@@ -0,0 +1,18 @@
+
+    // function changeLink() {
+    //     const reviewsElement = $('.reviews');
+    //     const reviewsLinks = reviewsElement.querySelector('.reviews-links');
+    //     const reviewsLinkList = reviewsLinks.querySelectorAll('.reviews-link');
+    //     reviewsLinkList.forEach(function(reviewsLinks) {
+
+    //         console.log(reviewsLinks);
+    //         reviewsLinks.onclick = function(e) {
+    //             e.target.classList.add('current');
+    //             if(reviewsLinks.has('current')) {
+    //                 reviewsLinks.classList.remove('current');
+    //             }
+    //         }
+    //     })
+    // }
+
+    // changeLink();
